20f4757408499cb78ec7b3b138dabeeb2bfb2e9c
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2026-09-21 19:13:08 -0300

    test: configura mocks de API com MSW e setup de testes

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,9 @@
+import { http, HttpResponse } from 'msw'
+export const handlers = [
+    http.get('/users', () => {
+        return HttpResponse.json([
+            { id: 1, name: 'John Doe' },
+            { id: 2, name: 'Jane Smith' }
+        ])
+    })
+]
